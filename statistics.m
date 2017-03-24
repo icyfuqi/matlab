63b6ae74a3e0724681228427a213e7a7307da1a2
tmachc <835892182@qq.com>
2017-03-24 12:31:33 +0800

    添加类统计

--- statistics.m
+++ statistics.m
@@ -1,11 +1,16 @@
-fileID1 = fopen('/Users/ccwonline/git/matlab/resultNum.txt');
-fileID2 = fopen('/Users/ccwonline/git/matlab/newlabel3129.txt');
+fileID1 = fopen('/Users/ccwonline/git/matlab/resultNum副本.txt');
+fileID2 = fopen('/Users/ccwonline/git/matlab/newlabel3129副本.txt');
 
 person = textscan(fileID2, '%s %s %s %s %s %s ');
 result = textscan(fileID1, '%s %s %s %s %s %s %s ');
 fclose(fileID1);
 fclose(fileID2);
 
+[hr, lr] = size(result{1});
+[hp, lp] = size(person{1});
+
+% 坐标误差
+error = 10;
 % 记录机器识别正确数
 resultRightNum = 0;
 % 记录抠图中被识别出的数
@@ -14,8 +19,10 @@ personDiscernNum = 0;
 discernWrongNum = 0;
 % 在抠图中标记处识别出来的图
 arr = zeros(hp,1);
-% 坐标误差
-error = 10;
+% 在识别图中记录每个类别的正确错误数 33个类
+arrType = zeros(33,3);
+
+arrType(:,1,1) = 100;
 
 for i = 1 : hr
     % result中的第i行  取出 name type 坐标
@@ -25,6 +32,17 @@ for i = 1 : hr
     yr = str2double(result{3}{i});
     widthr = str2double(result{4}{i});
     heightr = str2double(result{5}{i});
+    % 先把类别名存入 arrType
+    for k = 1 : length(arrType)
+        if (arrType(k,1) == str2double(typer))
+            break;
+        end
+        if (arrType(k,1) == 100)
+            arrType(k,1) = str2double(typer);
+            break;
+        end
+    end
+    
     % 跟person中的每条进行对比
     for j = 1 : hp
         namep = person{1}{j};
@@ -47,6 +65,7 @@ for i = 1 : hr
                 if (typer == typep)
                     % 类别一样 识别正确 计数+1
                     resultRightNum = resultRightNum + 1;
+                    arrType(k,2) = arrType(k,2) + 1;
                     disp(strcat(str, '识别正确'));
                     break;
                 else
@@ -62,18 +81,29 @@ for i = 1 : hr
         end
         if (j == hp)
             % 最后一个了 还不对
+            arrType(k,3) = arrType(k,3) + 1;
             disp(strcat(str, '识别错误 无此图或坐标不对'));
         end
     end
 end
 
 for j = 1 : hp
-    
     if (arr(j) ~= 0)
         personDiscernNum = personDiscernNum + 1;
     end
 end
 
+disp('*******************************************');
 disp(strcat('识别对的数是：', num2str(resultRightNum)));
 disp(strcat('识别出来的图数是：', num2str(personDiscernNum)));
 disp(strcat('类别错误的数是：', num2str(discernWrongNum)));
+
+disp('*******************************************');
+for f = 1 : length(arrType)
+    if (arrType(f,1) ~= 100)
+        disp(strcat('类别: ', num2str(arrType(f,1)), ' 正确: ', ...
+            num2str(arrType(f,2)), ' 错误: ', num2str(arrType(f,3))));
+    end
+end
+
+disp('**************      end     **************');
